d9b34a556ad2783b1f92ea5f0fd1999f92c42d0b
TonyChyi <tonychee1989@gmail.com>
2016-01-26 13:13:30 +0800

    lint

--- packageinfo.go
+++ packageinfo.go
@@ -16,8 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-/*Package xmms2go
-Xmms2Go is a Go binding for libxmmsclient.
+/*
+Package xmms2Go is a Go binding for libxmmsclient.
 It's easy to use and friendly for go developers.
 Just import me to use.
 
